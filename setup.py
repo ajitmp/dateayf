aa5964229a2020e8724030c83bb4544a2c38b541
ajitmp <ajit.megaproject@gmail.com>
2024-07-19 17:55:28 +0900

    converted to package format

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='dateayf',
+version='0.0.1',
+author='Dr Ajit Kumar',
+author_email='ajitkumar.pu@gmail.com',
+description='A small package to convert any date string into other given date string.',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
