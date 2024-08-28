27e7f24e936e2426ce0106a81aabb6c70916b929
Ajit <ajit.megaproject@gmail.com>
2024-08-28 18:33:41 +0900

    minor update with date formats

--- dateayf.py
+++ dateayf.py
@@ -3,9 +3,9 @@ import re
 #from dateutil.parser import parse
 
 
-DATE_FORMATS = ["%b %d '%y", "%m/%d/%Y", "%d/%b/%Y", "%Y-%m-%d","%m/%d/%Y","%d/%m/%Y", "%B %d, %Y", "%B %d %Y", "%d %B %Y","%d %B, %Y","%b. %d, %Y","%b %d, %Y","%d %b %Y","%d %b %y", "%d %b, %Y", "%m.%d.%Y","%m/%d/%Y"]
+DATE_FORMATS = ["%d.%m.%Y", "%b %d '%y", "%m/%d/%Y", "%d/%b/%Y", "%Y-%m-%d","%m/%d/%Y","%d/%m/%Y", "%B %d, %Y", "%B %d %Y", "%d %B %Y","%d %B, %Y","%b. %d, %Y","%b %d, %Y","%d %b %Y","%d %b %y", "%d %b, %Y", "%m.%d.%Y","%m/%d/%Y"]
 
-def find_suitable_format(input_text_date):  
+def find_suitable_format(input_text_date):
     for date_format in DATE_FORMATS:
         try:
             if datetime.strptime(input_text_date,date_format):
@@ -25,7 +25,7 @@ def __clean_st_nd_rd_from_date_old(input_date_str):
         return input_date_str.replace("nd","")
     else:
         return input_date_str.strip()
-    
+
 def __clean_st_nd_rd_from_date(input_date_str):
     #print(input_date_str)
     # Regular expression pattern to match any digit followed by "st", "nd", "rd", or "th"
@@ -38,7 +38,7 @@ def __clean_st_nd_rd_from_date(input_date_str):
         return input_date_str.strip()
 
 
-       
+
 
 
 def clean_date_str(input_date_str):
@@ -61,8 +61,8 @@ def clean_date_str(input_date_str):
 
 
 def convert_any_to_your_format(input_text_date, target_date):
-    #specail case :Python's %b typically matches "Sep", not "Sept.". 
-    # if input_text_date.lower().startswith("sept"):         
+    #specail case :Python's %b typically matches "Sep", not "Sept.".
+    # if input_text_date.lower().startswith("sept"):
     #     clean_input_text_date= input_text_date.strip().replace("Sept.", "Sep.")
     # else:
     #     clean_input_text_date= input_text_date.strip()
@@ -70,7 +70,7 @@ def convert_any_to_your_format(input_text_date, target_date):
     clean_target_date = target_date.strip()
     desired_format = find_suitable_format(clean_target_date)
     #print(desired_format)
-    input_format = find_suitable_format(clean_input_text_date)    
+    input_format = find_suitable_format(clean_input_text_date)
     #print(input_format)
     if desired_format and input_format:
         # Parse the date string into a datetime object
